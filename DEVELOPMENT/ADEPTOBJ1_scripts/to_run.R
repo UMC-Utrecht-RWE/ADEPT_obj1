73c3514e9d1604972826bac77e60d2d82a253e32
Magdalena <gamba_m@yahoo.com>
2025-12-16 14:45:39 +0100

    logging script added; clean up of to_run script
    DEAPS no longer need to comment/out scripts they need to run - now done based on deap name. Output to console now logged in text file saved in main scripts folder

--- DEVELOPMENT/ADEPTOBJ1_scripts/to_run.R
+++ DEVELOPMENT/ADEPTOBJ1_scripts/to_run.R
@@ -1,64 +1,82 @@
-# Clear all objects from current R environment to start with a clean workspace
+# Clear all objects from current R environment
 rm(list = ls())
 
-# Check if 'rstudioapi' package is installed; if not, install it, then load package
+# Install rstudioapi if not installed already, load package
 if (!require(rstudioapi)) install.packages("rstudioapi")
 
-# Get the folder path of the currently open R script in RStudio and set it as the working directory
+# Get the folder path of current script
 thisdir <- dirname(rstudioapi::getSourceEditorContext()$path)
 
 # ======================
-#     DEAP Selections
+#     DEAP SELECTIONS
 # ======================
 
 ### <<< USER INPUT >>> ###
-# Manually uncomment ONE line below to set your DEAP data source:
+#<<< ===========================================================================================================>>>
+# Select DEAP data source by uncommenting **only one** of the lines below.
 
 DEAP_data <- "BIFAP"
-# DEAP_data <- "CPRD"
-# DEAP_data <- "EFEMERIS"
-# DEAP_data <- "FIN_REG"
-# DEAP_data <- "NOR_REG"
-# DEAP_data <- "PHARMO"
-# DEAP_data <- "SIDIAP"
-# DEAP_data <- "VAL_PAD"
-# DEAP_data <- "VID"
+#DEAP_data <- "CPRD"
+#DEAP_data <- "EFEMERIS"
+#DEAP_data <- "FIN_REG"
+#DEAP_data <- "NOR_REG"
+#DEAP_data <- "PHARMO"
+#DEAP_data <- "SIDIAP"
+#DEAP_data <- "VAL_PAD"
+#DEAP_data <- "VID"
 
 ### <<< USER INPUT >>> ###
 #<<< ===========================================================================================================>>>
-# Set start study date - this is the date from which data is considered of good quality
-# Analysis counts will be done from start_study_date + lookback period.
-# In the default example, if start_study_date is 1999-01-01, then medicine counts will be done from 2000-01-01
+# Set start date of your study period.  
+# This is the date from which data is considered reliable and of good quality.  
+# Analysis counts start from start_study_date + lookback period.  
+# Example: If start_study_date = "1999-01-01", counts will be computed from "2000-01-01".
+
 start_study_date <- "YYYY-MM-DD"
 
+### <<< USER INPUT >>> ###
 #<<< ===========================================================================================================>>>
-# Set directory where CDM tables are
-CDM_dir <- "Path/To/Your/CDM/Folders/here"
+# Set directory paths
 
-#<<< ===========================================================================================================>>>
-# Set directory where D3_pregnancy_final.RData file is (created by Pregnancy Algorithm)
+# Path to folder with CDM tables 
+CDM_dir <- "Path/To/Your/CDM/Folder/here" 
+
+# Path to Pregnancies (D3_pregnancy_final.RData)
 preg_dir <- "Path/To/Your/Pregnancy/D3_pregnancy_final.RData/File/Here"
 
-#<<< ===========================================================================================================>>>
 
-# Run to set DEAP flags
+# ======================
+#     SCRIPTS 
+# ======================
+
+# Start logging
+source(file.path(thisdir, "p_steps", "logging.R"), local = TRUE)
+start_logging(file.path(thisdir, "log_file.txt"))
+
+# Set flags
 source(file.path(thisdir, "p_steps", "set_flags.R"), local = TRUE)
 
-# # === ANALYSIS SCRIPTS ===
-# Objectives 1.1 and 1.2
-source(file.path(thisdir, "p_steps", "run_analysis.R"), local = TRUE)
+# BIFAP, CPRD, NOR_REG, PHARMO, SIDIAP, VAL_PAD, VID
+if(!deap_flags$is_EFEMERIS || !deap_flags$is_FIN_REG){
+  source(file.path(thisdir, "p_steps", "run_analysis.R"), local = TRUE)                    #analysis obj 1.1, 1.2
+  source(file.path(thisdir, "p_steps", "run_analysis_pregnancies.R"), local = TRUE)        #analysis obj 1.3, 1.4
+  # Excluding VID
+  if(!deap_flags$is_VID){
+    source(file.path(thisdir, "p_steps", "calculate_weighted_daily_dose.R"), local = TRUE) #analysis obj 1.5
+  }
+  stop_logging() #stop logging
+}
 
-# Objectives 1.3 and 1.4
-source(file.path(thisdir, "p_steps", "run_analysis_pregnancies.R"), local = TRUE)
+# EFEMERIS only
+if(deap_flags$is_EFEMERIS){
+  source(file.path(thisdir, "p_steps", "run_analysis_EFEMERIS.R"), local = TRUE)           #analysis obj 1.3, 1.4, 1.5
+  stop_logging() #stop logging
+}
 
-# Objectives 1.5
-source(file.path(thisdir, "p_steps", "calculate_weighted_daily_dose.R"), local = TRUE)
+# FINLAND only
+if(deap_flags$is_FIN_REG){
+  source(file.path(thisdir, "p_steps", "run_analysis_FIN_REG.R"), local = TRUE)            #analysis obj 1.3, 1.4, 1.5
+  source(file.path(thisdir, "p_steps", "mask_counts_keep_rates_save_csv.R"), local = TRUE) #masking 
+  stop_logging() #stop logging
+}
 
-# EFEMERIS
-# source(file.path(thisdir, "p_steps", "run_analysis_EFEMERIS.R"), local = TRUE)
-
-# FIN_REG
-# source(file.path(thisdir, "p_steps", "run_analysis_FIN_REG.R"), local = TRUE)
-
-# mask and save as csv (FIN_REG ONLY!)
-# source(file.path(thisdir, "p_steps", "mask_and_save_as_csv.R"), local = TRUE)
