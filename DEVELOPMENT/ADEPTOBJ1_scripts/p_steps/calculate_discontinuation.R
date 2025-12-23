ae27b6642aefd7ed381558528025bfc0d5167211
Magdalena <gamba_m@yahoo.com>
2025-12-23 13:38:39 +0100

    general cleanup - 1.2 - discontinuation

--- DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_discontinuation.R
+++ DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_discontinuation.R
@@ -13,97 +13,99 @@ print("=========================================================================
 
 # List all episode files
 files_episodes <- list.files(file.path(paths$D3_dir, "tx_episodes"), pattern = "\\.rds$")
+files_episodes <- files_episodes[grepl(paste0("^", pop_prefix, "_"), files_episodes)] # only current pop_prefix
 
-if (!deap_flags$is_EFEMERIS && !deap_flags$is_FIN_REG) {
-  files_episodes <- files_episodes[grepl(paste0("^", pop_prefix, "_"), files_episodes)] # only current pop_prefix
-  # Prevalence files
-  files_prevalence_counts <- list.files(file.path(paths$D5_dir, "1.1_prevalence"), pattern = "\\.rds$")
-  files_prevalence_counts <- files_prevalence_counts[grepl(paste0("^", pop_prefix, "_"), files_prevalence_counts)]# only current pop_prefix
-}
+# Prevalence counts (denominator)
+files_prevalence_counts <- list.files(file.path(paths$D5_dir, "1.1_prevalence"), pattern = "\\.rds$")
+files_prevalence_counts <- files_prevalence_counts[grepl(paste0("^", pop_prefix, "_"), files_prevalence_counts)]# only current pop_prefix
 
 # Loop through each treatment episode file
 for (episode in seq_along(files_episodes)) {
+  
+  # Get name of current exposure
   episode_name <- gsub("_treatment_episode\\.rds$", "", files_episodes[episode])
-  # Read the treatment episode file
-  dt <- readRDS(file.path(paths$D3_dir, "tx_episodes", files_episodes[episode]))
+  
   # Print Message
   message("Processing: ", episode_name)
-  if (!deap_flags$is_EFEMERIS && !deap_flags$is_FIN_REG) {
-    # Remove duplicates
-    dt <- unique(dt, by = c("person_id", "episode.start", "episode.end"))
-    # Order episodes by person & start date
-    setorder(dt, person_id, episode.start)
-    # Get next episode start date
-    dt[, next_start := shift(episode.start, type = "lead"), by = .(person_id)]
-    # Flag discontinuation events
-    dt[, discontinuer_flag := fifelse(is.na(next_start), (exit_date - episode.end >= 120), (next_start - episode.end >= 120))]
-    # Keep only the discontinued episodes
-    discontinuers <- dt[discontinuer_flag == TRUE, ]
-    # Keep only episodes between entry and exit date
-    discontinuers <- discontinuers[episode.end >= start_follow_up & episode.end <= end_follow_up, ]
-    # Print message if no discontinuers found
-    if (nrow(discontinuers) == 0) {
-      message(red(paste("No discontinuers were found for:", episode_name)))
-      next
-    }
-  } else {
-    # Remove duplicates
-    dt <- unique(dt, by = c("pregnancy_id", "episode.start", "episode.end"))
-    # Order episodes by person & start date
-    setorder(dt, pregnancy_id,  episode.start)
-    # Get next episode start date
-    dt[, next_start := shift(episode.start, type = "lead"), by = .(pregnancy_id)]
-    # Flag discontinuation events
-    dt[, discontinuer_flag := fifelse(is.na(next_start), (end_follow_up - episode.end >= 120), (next_start - episode.end >= 120))]
-    # Keep only the discontinued episodes
-    discontinuers <- dt[discontinuer_flag == TRUE, ]
-    # Keep only episodes between entry and exit date
-    discontinuers <- discontinuers[episode.end >= start_follow_up & episode.end <= end_follow_up, ]
-    # Print message if no discontinuers found
-    if (nrow(discontinuers) == 0) {
-      message(red(paste("No discontinuers were found for:", episode_name)))
-      next
-    }
-    # Save discontinued data if present
-    if (nrow(discontinuers) > 0) saveRDS(discontinuers, file.path(paths$D4_dir, "1.2_discontinued", paste0(gsub("_treatment_episode\\.rds$", "", files_episodes[episode]), "_discontinued_data.rds")))
+  
+  # Read in treatment episode file
+  dt <- readRDS(file.path(paths$D3_dir, "tx_episodes", files_episodes[episode]))
+  dt <- unique(dt, by = c("person_id", "episode.start", "episode.end")) # Remove duplicates
+  
+  # Order episodes by person & start date
+  setorder(dt, person_id, episode.start)
+  
+  # Get next episode start date
+  dt[, next_start := shift(episode.start, type = "lead"), by = .(person_id)]
+  
+  # Flag discontinuation events
+  dt[, discontinuer_flag := fifelse(is.na(next_start), (exit_date - episode.end >= 120), (next_start - episode.end >= 120))]
+  
+  # Keep only the discontinued episodes
+  discontinuers <- dt[discontinuer_flag == TRUE, ]
+  
+  # Keep only episodes between entry and exit date
+  discontinuers <- discontinuers[episode.end >= start_follow_up & episode.end <= end_follow_up, ]
+  
+  # Print message if no discontinuers found
+  if (nrow(discontinuers) == 0) {
+    message(red(paste("No discontinuers were found for:", episode_name)))
     next
   }
-  # For DEAPS participating in Obj 1.1, 1.2
+  
   # Assign calendar year of each discontinued episode
   discontinuers[, year := year(episode.end)]
+  
   # Remove duplicates: Keep only one person id per year
   discontinuers <- unique(discontinuers, by = c("person_id", "year"))
+  
   # Count number of discontinuers per year
   discontinued_counts <- discontinuers[, .("N" = .N), by = year]
+  
   # Match corresponding prevalence file
   matched_prevalence_file <- files_prevalence_counts[gsub("_prevalence_counts\\.rds$", "", files_prevalence_counts) == gsub("_treatment_episode\\.rds$", "", files_episodes[episode])]
+  
   if (length(matched_prevalence_file) == 1) {
+    
     # Read in Prevalence file if file found
     prev_counts <- readRDS(file.path(paths$D5_dir, "1.1_prevalence", matched_prevalence_file))
+    
     # Prepare prevalence counts
     prev_counts[, c("n_total", "rate", "rate_computable") := NULL]
     setnames(prev_counts, "n_treated", "n_total")
+    
     # Merge discontinued with prevalence
     discontinued_all <- merge(discontinued_counts, prev_counts, by = "year", all.y = TRUE)
+    
     # Set N = 0 for years with no treatments
     discontinued_all[is.na(N), N := 0]
+    
     # Calculate discontinued as a rate (*100)
     discontinued_all[, rate := round(100 * N / n_total, 3)][N == 0 & n_total == 0, rate := 0]
+    
     # Set warnings if Numerator > than Denominator or if Denominator is 0 and Numerator is >0
     if (nrow(discontinued_all[N > n_total]) > 0) warning(red("Warning: Some numerator values exceed denominator."))
     if (nrow(discontinued_all[n_total == 0 & N != 0]) > 0) warning(red("Warning: Denominator zero with non-zero numerator."))
+    
     # Save data where odd values
     if (nrow(discontinued_all[N > n_total]) > 0) fwrite(discontinued_all[N > n_total], file.path(paths$D5_dir, "1.2_discontinued", paste0(episode_name, "_num_gt_denominator.csv")))
     if (nrow(discontinued_all[n_total == 0 & N != 0]) > 0) fwrite(discontinued_all[n_total == 0 & N != 0], file.path(paths$D5_dir, "1.2_discontinued", paste0(episode_name, "_denominator_zero_numerator_nonzero.csv")))
+    
     # Create column marking if rate is computable
     discontinued_all[, rate_computable := n_total > 0]
-    # rename columns
+    
+    # Rename columns
     setnames(discontinued_all, "N", "n_treated")
+    
     # Save discontinued data
     if (nrow(discontinuers) > 0) saveRDS(discontinuers, file.path(paths$D4_dir, "1.2_discontinued", paste0(gsub("_treatment_episode\\.rds$", "", files_episodes[episode]), "_discontinued_data.rds")))
+    
     # Save results
     saveRDS(discontinued_all, file.path(paths$D5_dir, "1.2_discontinued", paste0(gsub("_treatment_episode\\.rds$", "", files_episodes[episode]), "_discontinued_counts.rds")))
+    
   } else {
+    
     warning("No matching prevalence file found for: ", gsub("_treatment_episode\\.rds$", "", files_episodes[episode]))
+    
   }
 }
--- DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_discontinuation_stratification.R
+++ DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_discontinuation_stratification.R
@@ -10,70 +10,89 @@ print("=========================================================================
 print("========================= STRATIFYING DISCONTINUATION BY AGE GROUPS =========================")
 print("=============================================================================================")
 
-# create folder for stratification counts
+# Create folder for stratification counts
 dir.create(file.path(paths$D5_dir, "1.2_discontinued", "stratified"), showWarnings = FALSE, recursive = TRUE)
 
-# get list of discontinued files
+# Get list of discontinued files
 files_discontinued_episodes <- list.files(file.path(paths$D4_dir, "1.2_discontinued"), pattern = "\\.rds$")
 
-# filter for pop_prefix
+# Filter for pop_prefix
 files_discontinued_episodes <- files_discontinued_episodes[grepl(paste0("^", pop_prefix, "_"), files_discontinued_episodes)]
 
-# set strata levels
-# age groups
+# Set strata levels
+# Age groups
 age_levels <- c("12-18.99", "19-34.99", "35-54.99", "55-74.99", "75+", "UNKNOWN")
 
 # Create vector of study years from study dates (exist in environment)
 study_years <- seq(year(as.IDate(as.Date(start_study_date) + lookback_period)), year(as.IDate(end_study_date)))
 
-# create empty data frame using all possible years from the study for counts
+# Create empty data frame using all possible years from the study for counts
 all_combinations_agegroups   <- CJ(year = study_years, age_group = age_levels, unique = TRUE)
 
-# loop over files
+# Loop over files
 for (episode in seq_along(files_discontinued_episodes)) {
-  # get name of file being processed currently
+  
+  # Get name of file being processed currently
   file_name <- gsub("_discontinued_data\\.rds$", "", files_discontinued_episodes[episode])
-  # print message
+  
+  # Print message
   message("Processing: ", file_name)
-  # load current episode
+  
+  # Load current episode
   dt <- readRDS(file.path(paths$D4_dir, "1.2_discontinued", files_discontinued_episodes[episode]))
-  # prepare denominator
+  
+  # Prepare denominator
   denom_counts <- dt[, .(Freq = .N), by = year]
+  
   #<<< AGE GROUPS >>>#
-  # create a copy of dt for age group calculations
+  # Create a copy of dt for age group calculations
   agegroups <- copy(dt)
-  # convert dates to IDates
+  
+  # Convert dates to IDates
   agegroups[, birth_date := as.IDate(birth_date)][, episode.end := as.IDate(episode.end)]
-  # create column - age at episode end
+  
+  # Create column - age at episode end
   agegroups[, age_at_episode_end := floor(time_length(interval(birth_date, episode.end), unit = "years"))]
-  # create age groups
+  
+  # Create age groups
   agegroups[, age_group := fifelse(age_at_episode_end >= 12 & age_at_episode_end < 19, "12-18.99",
                                    fifelse(age_at_episode_end >= 19 & age_at_episode_end < 35, "19-34.99",
                                            fifelse(age_at_episode_end >= 35 & age_at_episode_end < 55, "35-54.99",
                                                    fifelse(age_at_episode_end >= 55 & age_at_episode_end < 75, "55-74.99",
                                                            fifelse(age_at_episode_end >= 75, "75+", "UNKNOWN")))))]
+  
   # Keep one row per person_id - episode.start
   agegroups <- unique(agegroups, by = c("person_id", "episode.start"))
-  # count groups per year
+  
+  # Count groups per year
   agegroup_counts <- agegroups[, .N, by = .(year, age_group)]
-  # merge with empty data frame to get all years
+  
+  # Merge with empty data frame to get all years
   agegroup_counts <- merge(all_combinations_agegroups, agegroup_counts, by = c("year", "age_group"), all.x = TRUE)
-  # if is.na(N), replace it with 0
+  
+  # If is.na(N), replace it with 0
   agegroup_counts[is.na(N), N := 0]
-  # merge with denominator
+  
+  # Merge with denominator
   agegroup_counts <- merge(agegroup_counts, denom_counts, by = c("year"), all.x = TRUE)
-  # if is.na(Freq), replace it with 0
+  
+  # If is.na(Freq), replace it with 0
   agegroup_counts[is.na(Freq), Freq := 0]
-  # calculate rate, if N = 0 and Freq = 0 then change the rate to 0
+  
+  # Calculate rate, if N = 0 and Freq = 0 then change the rate to 0
   agegroup_counts[, rate := round(100 * N / Freq, 3)][N == 0 & Freq == 0, rate := 0]
-  # create a column marking if rate is computable aka TRUE. It will be false if denominator is 0
+  
+  # Create a column marking if rate is computable aka TRUE. It will be false if denominator is 0
   agegroup_counts[, rate_computable := Freq > 0]
-  # sanity check
-  # sum counts per year
+  
+  # Sanity check
+  # Sum counts per year
   check_counts <- agegroup_counts[, .(sum_age_groups = sum(N), denominator = unique(Freq)), by = year]
-  # check for equality
+  
+  # Check for equality
   check_counts[, match := sum_age_groups == denominator]
-  # stop if any mismatch
+  
+  # Stop if any mismatch
   if (any(!check_counts$match)) {
     cat("\nError: Mismatch detected between numerator and denominator!\n")
     print(check_counts[match == FALSE])
@@ -81,5 +100,7 @@ for (episode in seq_along(files_discontinued_episodes)) {
   } else {
     message(blue("All age group counts match the denominator for every year"))
   }
+  
+  # Save file 
   saveRDS(agegroup_counts, file.path(paths$D5_dir, "1.2_discontinued", "stratified", paste0(file_name, "_discontinued_agegroup_counts.rds")))
 }
