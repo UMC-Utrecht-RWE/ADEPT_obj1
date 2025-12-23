4f7640f338498f642393ad7e0ee6cc4f4892c953
Magdalena <gamba_m@yahoo.com>
2025-12-23 14:03:31 +0100

    made foverlaps join explicit

--- DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_polytherapy.R
+++ DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_polytherapy.R
@@ -18,75 +18,71 @@ exclude <- c("DP_ANTIEPINEW", "DP_ANTIEPIOLD", "DP_BENZOANTIEPILEPTIC", "DP_GABA
 files_episodes <- list.files(file.path(paths$D3_dir, "tx_episodes"), pattern = "\\.rds$")
 files_episodes <- files_episodes[!(gsub(paste0("^", pop_prefix, "_|_treatment_episode\\.rds$"), "", files_episodes) %in% exclude)] # exclude subgroups
 
-# Load denominator file
 if (!deap_flags$is_EFEMERIS && !deap_flags$is_FIN_REG) {
-  files_episodes <- files_episodes[grepl(paste0("^", pop_prefix, "_"), files_episodes)] # Filters female vs Male
-  # Load denominator file
-  denominator <- readRDS(file.path(paths$D3_dir, "denominator", paste0(pop_prefix, "_denominator.rds")))
+  files_episodes <- files_episodes[grepl(paste0("^", pop_prefix, "_"), files_episodes)] # gets the female files 
+  denominator <- readRDS(file.path(paths$D3_dir, "denominator", paste0(pop_prefix, "_denominator.rds"))) # denominator file 
 }
 
-
 for (epi1 in seq_along(files_episodes)) {
+  
   for (epi2 in seq_along(files_episodes)) {
+    
     # Skip same or duplicate pairs
     if (epi1 >= epi2) next
+    
     # Save names of current episodes being processed
     name_epi1 <- sub("_treatment_episode\\.rds$", "", basename(files_episodes[epi1]))
     name_epi2 <- sub("_treatment_episode\\.rds$", "", basename(files_episodes[epi2]))
+    
     # Load treatment episodes
     dt1 <- as.data.table(readRDS(file.path(paths$D3_dir, "tx_episodes", files_episodes[epi1])))
     dt2 <- as.data.table(readRDS(file.path(paths$D3_dir, "tx_episodes", files_episodes[epi2])))
+    
     if (!deap_flags$is_EFEMERIS && !deap_flags$is_FIN_REG) {
+      
       # Remove duplicates
       dt1 <- unique(dt1, by = c("person_id", "episode.start"))
       dt2 <- unique(dt2, by = c("person_id", "episode.start"))
+      
       # Drop unnecessary columns
       dt1[, c("episode.ID", "end.episode.gap.days", "episode.duration") := NULL]
       dt2[, c("episode.ID", "end.episode.gap.days", "episode.duration") := NULL]
+      
       # Create window start and window end columns - periods of overlap
       dt1[, window_start := episode.start][, window_end := episode.end]
       dt2[, window_start := episode.start][, window_end := episode.end]
+      
       # Keep only overlapping person_ids
       common_ids <- intersect(dt1[, unique(person_id)], dt2[, unique(person_id)])
+      
       if (length(common_ids) == 0) {
         message(red(paste("No Polytherapy found between:", name_epi1, "and", name_epi2)))
         next
       }
+      
+      # Subset data to contain id's common to both data sets
       dt1_sub <- dt1[person_id %in% common_ids]
       dt2_sub <- dt2[person_id %in% common_ids]
+      
       # Set keys
       setkey(dt1_sub, person_id, window_start, window_end)
       setkey(dt2_sub, person_id, window_start, window_end)
-    } else {
-      # Remove duplicates
-      dt1 <- unique(dt1, by = c("pregnancy_id", "episode.start"))
-      dt2 <- unique(dt2, by = c("pregnancy_id", "episode.start"))
-      # Drop unnecessary columns
-      dt1[, c("episode.ID", "end.episode.gap.days", "episode.duration") := NULL]
-      dt2[, c("episode.ID", "end.episode.gap.days", "episode.duration") := NULL]
-      # Create window start and window end columns - periods of overlap
-      dt1[, window_start := episode.start][, window_end := episode.end]
-      dt2[, window_start := episode.start][, window_end := episode.end]
-      # Keep only overlapping pregnancy_ids
-      common_ids <- intersect(dt1[, unique(pregnancy_id)], dt2[, unique(pregnancy_id)])
-      if (length(common_ids) == 0) {
-        message(red(paste("No Polytherapy found between:", name_epi1, "and", name_epi2)))
-        next
-      }
-      dt1_sub <- dt1[pregnancy_id %in% common_ids]
-      dt2_sub <- dt2[pregnancy_id %in% common_ids]
-      # Set keys
-      setkey(dt1_sub, pregnancy_id, window_start, window_end)
-      setkey(dt2_sub, pregnancy_id, window_start, window_end)
-    }
-    # COMMON TO BOTH
-    # Find overlaps
-    overlaps <- foverlaps(dt1_sub, dt2_sub, type = "any", nomatch = 0L)
-    # Calculate overlap duration
-    overlaps[, overlap_start := pmax(window_start, i.window_start)]
-    overlaps[, overlap_end   := pmin(window_end, i.window_end)]
-    overlaps[, overlap_days  := as.numeric(overlap_end - overlap_start) + 1]
-    if (!deap_flags$is_EFEMERIS && !deap_flags$is_FIN_REG) {
+      
+      # Find overlaps
+      overlaps <- foverlaps(
+        x = dt1_sub,
+        y = dt2_sub,
+        by.x = c("person_id", "window_start", "window_end"),
+        by.y = c("person_id", "window_start", "window_end"),
+        type = "any",
+        nomatch = 0L
+      )
+
+      # Calculate overlap duration
+      overlaps[, overlap_start := pmax(window_start, i.window_start)]
+      overlaps[, overlap_end   := pmin(window_end, i.window_end)]
+      overlaps[, overlap_days  := as.numeric(overlap_end - overlap_start) + 1]
+      
       # The filtering is just for the message
       # We will save the dataset with all the overlaps and filter before counts or before using in for 1.4
       if (nrow(overlaps[overlap_days >= 182, ]) == 0) {
@@ -95,8 +91,53 @@ for (epi1 in seq_along(files_episodes)) {
       } else {
         message("Polytherapy found between: ", name_epi1, " and ", name_epi2)
       }
+      
     } else {
-      # EFEMERIS AND FIN_REG - we only need this for obj 1.4, where there needs to be an overlap of 3 months with pregnancy
+      
+      # Remove duplicates
+      dt1 <- unique(dt1, by = c("pregnancy_id", "episode.start"))
+      dt2 <- unique(dt2, by = c("pregnancy_id", "episode.start"))
+      
+      # Drop unnecessary columns
+      dt1[, c("episode.ID", "end.episode.gap.days", "episode.duration") := NULL]
+      dt2[, c("episode.ID", "end.episode.gap.days", "episode.duration") := NULL]
+      
+      # Create window start and window end columns - periods of overlap
+      dt1[, window_start := episode.start][, window_end := episode.end]
+      dt2[, window_start := episode.start][, window_end := episode.end]
+      
+      # Keep only overlapping pregnancy_ids
+      common_ids <- intersect(dt1[, unique(pregnancy_id)], dt2[, unique(pregnancy_id)])
+      
+      if (length(common_ids) == 0) {
+        message(red(paste("No Polytherapy found between:", name_epi1, "and", name_epi2)))
+        next
+      }
+      
+      # Subset data to contain id's common to both data sets
+      dt1_sub <- dt1[pregnancy_id %in% common_ids]
+      dt2_sub <- dt2[pregnancy_id %in% common_ids]
+      
+      # Set keys
+      setkey(dt1_sub, pregnancy_id, window_start, window_end)
+      setkey(dt2_sub, pregnancy_id, window_start, window_end)
+      
+      # Find overlaps
+      overlaps <- foverlaps(
+        x = dt1_sub,
+        y = dt2_sub,
+        by.x = c("pregnancy_id", "window_start", "window_end"),
+        by.y = c("pregnancy_id", "window_start", "window_end"),
+        type = "any",
+        nomatch = 0L
+      )
+      
+      # Calculate overlap duration
+      overlaps[, overlap_start := pmax(window_start, i.window_start)]
+      overlaps[, overlap_end   := pmin(window_end, i.window_end)]
+      overlaps[, overlap_days  := as.numeric(overlap_end - overlap_start) + 1]
+      
+      # We only need this for obj 1.4, where there needs to be an overlap of 3 months with pregnancy
       if (nrow(overlaps[overlap_days >= 91, ]) == 0) {
         message(red(paste("No Polytherapy found between:", name_epi1, "and", name_epi2)))
         next
@@ -104,18 +145,23 @@ for (epi1 in seq_along(files_episodes)) {
         message("Polytherapy found between: ", name_epi1, " and ", name_epi2)
       }
     }
+    
     # Save overlapping data in temp folder - Please note that these are all overlapping records regardless of the length of the overlap
     saveRDS(overlaps, file = file.path(paths$D3_dir, "tmp", paste0(name_epi1, "_to_", name_epi2, ".rds")))
+    
   }
 }
+
 ### Read back all files in temp folder
 files_overlaps  <- list.files(file.path(paths$D3_dir, "tmp"), pattern = "\\.rds$")
 
 # ===================== OVERALL POLYTHERAPY RATE =========================
 # Combine all the temp polytherapy files in one and save (all DEAP's)
 if (length(files_overlaps) > 0) {
+  
   # Read and combine all pairwise overlap files
   all_overlaps <- as.data.table(rbindlist(lapply(file.path(paths$D3_dir, "tmp", files_overlaps), readRDS), use.names = TRUE, fill = TRUE))
+  
   # Drop duplicate cols or cols you no longer need
   cols_to_drop <- c(
     "window_start", "window_end", "i.window_start", "i.window_end",
@@ -125,82 +171,118 @@ if (length(files_overlaps) > 0) {
     "episode.start", "i.episode.start", "episode.end", "i.episode.end",
     "code", "i.code"
   )
+  
   # Drop only those that actually exist in your data.table
   cols_to_drop <- intersect(cols_to_drop, names(all_overlaps))
+  
   # Remove them in place
   all_overlaps[, (cols_to_drop) := NULL]
+  
   # Drop only those that actually exist in your data.table
   cols_to_drop <- intersect(cols_to_drop, names(all_overlaps))
+  
   # Remove them in place
   all_overlaps[, (cols_to_drop) := NULL]
+  
   # Ensure overlap dates are IDate
   all_overlaps[, `:=`(overlap_start = as.IDate(overlap_start), overlap_end = as.IDate(overlap_end))]
+  
   # Assign year to each overlap start
   all_overlaps[, year := year(overlap_start)]
+  
   # This saves all overlaps regardless of length of overlap - this will be used in obj 1.4
   saveRDS(all_overlaps, file.path(paths$D4_dir, "1.2_polytherapy", paste0(pop_prefix, "_polytherapy_data.rds")))
+  
   # Clean out tmp folder
   if (length(list.files(file.path(paths$D3_dir, "tmp"), full.names = TRUE)) > 0) unlink(list.files(file.path(paths$D3_dir, "tmp"), full.names = TRUE))
 }
+
 # COUNTS
 if (!deap_flags$is_EFEMERIS && !deap_flags$is_FIN_REG) {
+  
   # Overlap should be between start and end fu
   all_overlaps <- all_overlaps[overlap_start >= start_follow_up & overlap_start <= end_follow_up & overlap_end >= start_follow_up & overlap_end <= end_follow_up]
+  
   # Since we are no longer filtering in the previous part, we need to do it here!
   all_overlaps_182 <- all_overlaps[overlap_days >= 182, ]
+  
   # Sort by person id and overlap start
   setorder(all_overlaps_182, person_id, overlap_start)
+  
   # Keep only one row per person per year
   all_overlaps_unique_182 <- unique(all_overlaps_182, by = c("person_id", "year"))
+  
   # Count unique individuals per year
   overall_counts_182 <- all_overlaps_unique_182[, .N, by = year]
+  
   # Merge with denominator
   overlap_all_182 <- merge(overall_counts_182, denominator, by = "year", all.y = TRUE)
+  
   # Handle missing numerator values
   overlap_all_182[is.na(N), N := 0]
+  
   # Compute polytherapy rate per 1000 persons
   overlap_all_182[, rate := round(1000 * N / Freq, 3)]
   overlap_all_182[N == 0 & Freq == 0, rate := 0]
+  
   # Set warnings if Numerator > than Denominator or if Denominator is 0 and Numerator is >0
   if (nrow(overlap_all_182[N > Freq]) > 0) warning(red("Warning: Some numerator values exceed denominator."))
   if (nrow(overlap_all_182[Freq == 0 & N != 0]) > 0) warning(red("Warning: Denominator zero with non-zero numerator."))
+  
   # Save data where odd values
   if (nrow(overlap_all_182[N > Freq]) > 0) fwrite(overlap_all[N > Freq], file.path(paths$D5_dir, "1.2_polytherapy",  "polytherapy_num_gt_denominator.csv"))
   if (nrow(overlap_all_182[Freq == 0 & N != 0]) > 0) fwrite(overlap_all[Freq == 0 & N != 0], file.path(paths$D5_dir, "1.2_polytherapy", "polytherapy_denominator_zero_numerator_nonzero.csv"))
+  
   # Create column marking if rate is computable
   overlap_all_182[, rate_computable := Freq > 0]
+  
   # Rename columns
   setnames(overlap_all_182, c("N", "Freq"), c("n_treated", "n_total"))
+  
   # Save results
   saveRDS(overlap_all_182, file.path(paths$D5_dir, "1.2_polytherapy", paste0(pop_prefix, "_OVERALL_polytherapy_counts_182.rds")))
+  
   # Sensitivity analysis overlap of 120 days
   # Since we are no longer filtering in the previous part, we need to do it here!
   all_overlaps_120 <- all_overlaps[overlap_days >= 120, ]
+  
   # Sort by person id and overlap start
   setorder(all_overlaps_120, person_id, overlap_start)
+  
   # Keep only one row per person per year
   all_overlaps_unique_120 <- unique(all_overlaps_120, by = c("person_id", "year"))
+  
   # Count unique individuals per year
   overall_counts_120 <- all_overlaps_unique_120[, .N, by = year]
+  
   # Merge with denominator
   overlap_all_120 <- merge(overall_counts_120, denominator, by = "year", all.y = TRUE)
+  
   # Handle missing numerator values
   overlap_all_120[is.na(N), N := 0]
+  
   # Compute polytherapy rate per 1000 persons
   overlap_all_120[, rate := round(1000 * N / Freq, 3)]
   overlap_all_120[N == 0 & Freq == 0, rate := 0]
+  
   # Set warnings if Numerator > than Denominator or if Denominator is 0 and Numerator is >0
   if (nrow(overlap_all_120[N > Freq]) > 0) warning(red("Warning: Some numerator values exceed denominator."))
   if (nrow(overlap_all_120[Freq == 0 & N != 0]) > 0) warning(red("Warning: Denominator zero with non-zero numerator."))
+  
   # Save data where odd values
   if (nrow(overlap_all_120[N > Freq]) > 0) fwrite(overlap_all[N > Freq], file.path(paths$D5_dir, "1.2_polytherapy",  "polytherapy_num_gt_denominator.csv"))
   if (nrow(overlap_all_120[Freq == 0 & N != 0]) > 0) fwrite(overlap_all[Freq == 0 & N != 0], file.path(paths$D5_dir, "1.2_polytherapy", "polytherapy_denominator_zero_numerator_nonzero.csv"))
+  
   # Create column marking if rate is computable
   overlap_all_120[, rate_computable := Freq > 0]
+  
   # Rename columns
   setnames(overlap_all_120, c("N", "Freq"), c("n_treated", "n_total"))
+  
   # create folder for stratification counts
   dir.create(file.path(paths$D5_dir, "1.2_polytherapy", "sensitivity"), showWarnings = FALSE, recursive = TRUE)
+  
   # Save results
   saveRDS(overlap_all_120, file.path(paths$D5_dir, "1.2_polytherapy", "sensitivity", paste0(pop_prefix, "_OVERALL_polytherapy_counts_120.rds")))
+  
 }
--- DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_polytherapy_stratification.R
+++ DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/calculate_polytherapy_stratification.R
@@ -10,36 +10,37 @@ print("=========================================================================
 print("========================= STRATIFYING POLYTHERAPY BY INDICATION =========================")
 print("=========================================================================================")
 
-# create folder for stratification counts
+# Create folder for stratification counts
 dir.create(file.path(paths$D5_dir, "1.2_polytherapy", "stratified"), showWarnings = FALSE, recursive = TRUE)
 
-# get list of polytherapy files
+# Get list of polytherapy files
 files_polytherapy_episodes <- list.files(file.path(paths$D4_dir, "1.2_polytherapy"), pattern = "\\.rds$")
 
-# filter for pop_prefix
+# Filter for pop_prefix
 files_polytherapy_episodes <- files_polytherapy_episodes[grepl(paste0("^", pop_prefix, "_"), files_polytherapy_episodes)]
 
-# get a list of indication files
+# Get a list of indication files
 files_indication <- list.files(file.path(paths$D3_dir, "indication"), pattern = "\\.rds$", full.names = TRUE)
 
-# filter for pop_prefix
+# Filter for pop_prefix
 files_indication <- files_indication[grepl(paste0("^", pop_prefix, "_"), basename(files_indication))]
 
-# load and bind all indications into one dataset, remove true duplicates
+# Load and bind all indications into one dataset, remove true duplicates
 dt_indication <- unique(rbindlist(lapply(files_indication, readRDS), use.names = TRUE, fill = TRUE))
 
-# change value of column event_definition in any rows with O_NEUROPATHICPAIN_COV or O_FIBROMYALGIA_AESI to algorithm name O_NEUROPATHICPAINALG_COV
+# Change value of column event_definition in any rows with O_NEUROPATHICPAIN_COV or O_FIBROMYALGIA_AESI to algorithm name O_NEUROPATHICPAINALG_COV
 dt_indication[event_definition == "O_NEUROPATHICPAIN_COV" | event_definition == "O_FIBROMYALGIA_AESI", event_definition := "O_NEUROPATHICPAINALG_COV"]
 
-# set strata levels
-# indications
+# Set strata levels
+# Indications
 indication_levels <- c("M_RESTLESSLEG_COV", "Ment_ANXIETY_COV", "Ment_BIPOLAR_AESI", "Ment_DEPRESSION_COV", "Ment_SCHIZOPHRENIA_COV",
                        "N_CONVULSION_AESI", "N_EPILEPSY_COV", "N_ESSENTIALTREMOR_AESI", "N_MIGRAINE_COV", "O_NEUROPATHICPAINALG_COV", "UNKNOWN")
+
 
 # Create vector of study years from study dates (exist in environment)
 study_years <- seq(year(as.IDate(as.Date(start_study_date) + lookback_period)), year(as.IDate(end_study_date)))
 
-# create empty data frame using all possible years from the study for counts
+# Create empty data frame using all possible years from the study for counts
 all_combinations_indications <- CJ(year = study_years, indication = indication_levels, unique = TRUE)
 
 # Load and bind all polytherapy files
@@ -48,7 +49,7 @@ dt <- rbindlist(lapply(file.path(paths$D4_dir, "1.2_polytherapy", files_polyther
 # Overlap should be between start and end fu
 dt <- dt[overlap_start >= start_follow_up & overlap_start <= end_follow_up & overlap_end >= start_follow_up & overlap_end <= end_follow_up]
 
-# keep records where overlap is greater or equal to 182 days
+# Keep records where overlap is greater or equal to 182 days
 dt <- dt[overlap_days >= 182, ]
 
 # Sort by person id and overlap start
@@ -57,25 +58,25 @@ setorder(dt, person_id, overlap_start)
 # Keep only one row per person per year
 dt <- unique(dt, by = c("person_id", "year"))
 
-# prepare denominator
+# Prepare denominator
 denom_counts <- dt[, .(Freq = .N), by = year]
 
 #<<< INDICATIONS >>>#
-# create a copy of dt for indication calculations
+# Create a copy of dt for indication calculations
 dt_temp <- copy(dt)
 
-# prepare data for foverlaps
-# polytherapy
+# Prepare data for foverlaps
+# Polytherapy
 dt_temp[, start_window := as.IDate(as.Date(overlap_start) %m-% lookback_period)][, end_window := overlap_start]
 
-# indication file
+# Indication file
 dt_indication[, start_event := as.IDate(event_date)][, end_event   := as.IDate(event_date)]
 
-# set keys
+# Set keys
 setkey(dt_temp, person_id, start_window, end_window)
 setkey(dt_indication, person_id, start_event, end_event)
 
-# perform overlap join
+# Perform overlap join
 indications <- foverlaps(dt_temp[, .(person_id, start_window, end_window, overlap_start, year)],
                          dt_indication[, .(person_id, start_event, end_event, event_date, event_definition)],
                          by.x = c("person_id", "start_window", "end_window"),
@@ -83,15 +84,14 @@ indications <- foverlaps(dt_temp[, .(person_id, start_window, end_window, overla
                          nomatch = NA
 )
 
-# calculate difference in days between episode start and event date of indication
+# Calculate difference in days between episode start and event date of indication
 indications[, diff_days := as.numeric(difftime(overlap_start, event_date, units = "days"))]
 
-# create column indication:
+# Create column indication:
 # if more than one rx is present, and epilepsy is among them, then priority is epilepsy
 # if any other rx are present, pick the one closest to episode.start
 # if one rx is present then that is the indication
 # if no indication is present within the 365 days before, then indication is unknown
-
 indications <- indications[
   , {
     if ("N_EPILEPSY_COV" %in% event_definition) {
@@ -114,29 +114,29 @@ indications <- indications[
 # Keep one row per person_id - overlap.start
 indications <- unique(indications, by = c("person_id", "overlap_start"))
 
-# count groups per year
+# Count groups per year
 indication_counts <- indications[, .N, by = .(year, indication)]
 
-# merge counts with empty dt
+# Merge counts with empty dt
 indication_counts <- merge(all_combinations_indications, indication_counts, by = c("year", "indication"), all.x = TRUE)
 
-# if is.na(N), replace it with 0
+# If is.na(N), replace it with 0
 indication_counts[is.na(N), N := 0]
 
-# merge with denominator
+# Merge with denominator
 indication_counts <- merge(indication_counts, denom_counts, by = c("year"), all.x = TRUE)
 
-# if is.na(Freq), replace it with 0
+# If is.na(Freq), replace it with 0
 indication_counts[is.na(Freq), Freq := 0]
 
-# calculate rate, if N = 0 and Freq = 0 then change the rate to 0
+# Calculate rate, if N = 0 and Freq = 0 then change the rate to 0
 indication_counts[, rate := round(100 * N / Freq, 3)][N == 0 & Freq == 0, rate := 0]
 
-# create a column marking if rate is computable aka TRUE. It will be false if denominator is 0
+# Create a column marking if rate is computable aka TRUE. It will be false if denominator is 0
 indication_counts[, rate_computable := Freq > 0]
 
-# sanity check
-# sum counts per year
+# Sanity check
+# Sum counts per year
 check_counts <- indication_counts[, .(sum_indications = sum(N), denominator = unique(Freq)), by = year]
 
 # Check for equality
@@ -151,5 +151,5 @@ if (any(!check_counts$match)) {
   message(blue("All indication counts match the denominator for every year"))
 }
 
-# save counts
+# Save counts
 saveRDS(indication_counts, file.path(paths$D5_dir, "1.2_polytherapy", "stratified", paste0(pop_prefix, "_polytherapy_indication_counts.rds")))
