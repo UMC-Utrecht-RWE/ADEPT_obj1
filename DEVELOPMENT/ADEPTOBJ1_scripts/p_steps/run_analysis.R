5ab763cb04ef89d686ba701b01360dee2cad2e42
Magdalena <gamba_m@yahoo.com>
2026-01-22 15:09:24 +0100

    general cleanup

--- DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/run_analysis.R
+++ DEVELOPMENT/ADEPTOBJ1_scripts/p_steps/run_analysis.R
@@ -61,7 +61,7 @@ for (pop in seq_along(populations)){
     
     # Move algorithm inputs to folders
     source(file.path(thisdir, "p_steps", "move_files_to_folders.R"), local = TRUE)
-    
+
     # Comorbidity and Indication Counts
     source(file.path(thisdir, "p_steps", "calculate_indication_and_comorbidities.R"), local = TRUE)
 
